94b8f79ee57489d2d5324241956e67f4d2b5dfff
AlvaroXXDaw <alvaro.alvaro601@gmail.com>
2025-10-07 18:12:18 +0200

    3

--- formularios/form3.php
+++ formularios/form3.php
@@ -6,9 +6,9 @@
 <input type="checkbox" id="vehicle1" name="vehicle[]" value="bike">
 <label for="vehicle1">Ive got a bike</label><br>
 <input type="checkbox" id="vehicle2" name="vehicle[]" value="car">
-<label for="vehicle2">Ive got a bike</label><br>
+<label for="vehicle2">Ive got a car</label><br>
 <input type="checkbox" id="vehicle3" name="vehicle[]" value="boat">
-<label for="vehicle3">Ive got a bike</label><br>
+<label for="vehicle3">Ive got a boat</label><br>
 <input type="submit" name="submitButton" id="submitButton" value="Submit Form">
 </form>
 
